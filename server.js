b0fbfd8d7426f3dd8e6e0544a2ebc22cecec54c5
mazefernandez <aafernandez5@up.edu.ph>
2023-09-06 16:28:36 +0800

    Add error handling and db connection to server

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express') 
+const connection = require('./db')
 
 const app = express()
 const PORT = 3000
+
+connection()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Simpletrade Ventures Exam')
 })
 
-app.listen(PORT, () => 
+const server = app.listen(PORT, () => 
     console.log(`Server is connected to port ${PORT}`)
 )
+
+// Error handling 
+process.on("unhandledRejection", err => {
+    console.log(`Error occured: ${err.message}`)
+    server.close(() => process.exit(1))
+})
